31f7e66f65436281ea4dd71e04b4e0d3f249d55b
JaedanC <jae.cal@hotmail.com>
2024-06-30 01:09:32 +1000

    Collision Groups

--- game.py
+++ game.py
@@ -2,90 +2,63 @@ import pygame
 from pygame.locals import (QUIT, KEYDOWN, K_ESCAPE, K_a, K_d, K_w, K_s, KEYUP, K_q)
 pygame.color.Color
 import random
+from enum import Flag
 
 # See https://raw.githubusercontent.com/pybox2d/pybox2d/master/library/Box2D/examples/simple/simple_01.py
 import Box2D
 from model import *
+from settings import *
 
-TARGET_FPS = 60
-TIME_STEP = 1.0 / TARGET_FPS
 
-# --- pybox2d world setup ---
 # Create the world
-world = Box2D.b2World(gravity=(0, 0), doSleep=True)
+world = Box2D.b2World(gravity=(0, -10), doSleep=True)
 
+kinematic_rect = KinematicRectangle(world, (100, 350), (200, 50),                                (100, 100, 250, 255))
+dynamic_rect =   DynamicRectangle(  world, (200, 250), (25, 45), CollisionInfo(restitution=0.7), (100, 150, 150, 255))
 
-world_borders = [
-    StaticLine(world, (5,                5                ), (SCREEN_WIDTH - 5, 5                ), colour=(255, 150, 100, 255)),
-    StaticLine(world, (SCREEN_WIDTH - 5, 5                ), (SCREEN_WIDTH - 5, SCREEN_HEIGHT - 5), colour=(255, 150, 100, 255)),
-    StaticLine(world, (SCREEN_WIDTH - 5, SCREEN_HEIGHT - 5), (5,                SCREEN_HEIGHT - 5), colour=(255, 150, 100, 255)),
-    StaticLine(world, (5,                SCREEN_HEIGHT - 5), (5,                5                ), colour=(255, 150, 100, 255)),
-]
+kinematic_circle = KinematicCircle(world, (370, 275), 25,                                 (50, 200, 250, 255))
+mouse_ball =       KinematicCircle(world, (500, 150), 10                                                     )
+dynamic_circle =   DynamicCircle(  world, (150, 250), 30, CollisionInfo(restitution=0.7), (100, 255, 150, 255)).set_velocity((350, -450))
 
-game_balls = [
-    DynamicCircle(
-        world,
-        (150, 250),
-        30,
-        CollisionInfo(restitution=1.1),
-        colour=(100, 255, 150, 255),
-    ).set_velocity((350, -450)),
-    StaticCircle(
-        world,
-        (320, 275),
-        25,
-        colour=(50, 255, 150, 255),
-    ).set_velocity((35, -45)),
-    KinematicCircle(
-        world,
-        (370, 275),
-        25,
-        colour=(50, 200, 250, 255),
-    ).set_velocity((-35, 45))
-]
+kinematic_line = KinematicLine(world, (30, 20), (130, 100), (255, 90, 255, 255))
 
-dynamic_rect = DynamicRectangle(
-    world,
-    (200, 250),
-    (25, 45),
-    CollisionInfo(restitution=0.9),
-    (100, 100, 150, 255)
-)
-
-static_rect = StaticRectangle(
-    world,
-    (230, 250),
-    (25, 45),
-    (100, 100, 150, 255)
-)
-
-kinematic_rect = KinematicRectangle(
-    world,
-    (100, 350),
-    (200, 50),
-    (100, 100, 250, 255)
-)
-
-kinematic_line = KinematicLine(
-    world,
-    (30, 20),
-    (130, 100),
-    (255, 90, 255, 255),
-)
-
-game_objects = game_balls + world_borders + [
+game_objects = [
+    StaticLine(world, (10,                10                ), (SCREEN_WIDTH - 10, 10                ), colour=(255, 150, 100, 255)),
+    StaticLine(world, (SCREEN_WIDTH - 10, 10                ), (SCREEN_WIDTH - 10, SCREEN_HEIGHT - 10), colour=(255, 150, 100, 255)),
+    StaticLine(world, (SCREEN_WIDTH - 10, SCREEN_HEIGHT - 10), (10,                SCREEN_HEIGHT - 10), colour=(255, 150, 100, 255)),
+    StaticLine(world, (10,                SCREEN_HEIGHT - 10), (10,                10                ), colour=(255, 150, 100, 255)),
+    DynamicLine(world, (10, 10), (100, 10), CollisionInfo(restitution=0.4), (255, 255, 90, 255)),
+    StaticCircle(   world, (320, 275), 25, (50, 255, 150, 255)).set_velocity(( 35, -45)),
+    StaticRectangle(world, (230, 250), (25, 45), (100, 100, 150, 255)),
     dynamic_rect,
-    static_rect,
     kinematic_rect,
-    DynamicLine(
-        world,
-        (10, 10),
-        (100, 100),
-        CollisionInfo(restitution=0.9),
-        (255, 255, 90, 255),
-    ),
-    kinematic_line
+    kinematic_circle,
+    dynamic_circle,
+    kinematic_line,
+    mouse_ball,
 ]
+
+
+# region Collision Groups
+
+class CollisionGroup(Flag):
+    NoCollision = 0
+    Default = auto()
+    SecondGroup = auto()
+    ThirdGroup = auto()
+
+    @classmethod
+    def ALL(cls):
+        # From https://stackoverflow.com/a/78229655
+        return ~CollisionGroup(0)
+
+for game_obj in game_objects:
+    game_obj.set_collision_group(CollisionGroup.Default, CollisionGroup.ALL())
+
+dynamic_circle.set_collision_group(CollisionGroup.SecondGroup, CollisionGroup.Default)
+mouse_ball.set_collision_group(CollisionGroup.ThirdGroup,      CollisionGroup.Default)
+
+# endregion
 
 
 def main():
@@ -93,45 +66,50 @@ def main():
     pygame.display.set_caption("Simple pygame example")
     clock = pygame.time.Clock()
     
-    key_query = KeyQuery()
-    shape_register = ShapeRegister(world)
-    shape_register.add(game_objects)
+    io = KeyQuery()
+    shape_registry = ShapeRegistry(world)
+    shape_registry.add(game_objects)
     
     running = True
     while running:
-        key_query.clear()
+        io.clear_pressed()
+        io.mark_mouse_relative(pygame.mouse.get_rel())
         for event in pygame.event.get():
             if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
                 running = False
                 break
 
             if event.type == KEYDOWN:
-                key_query.mark_pressed(event.key)
+                io.mark_pressed(event.key)
             elif event.type == KEYUP:
-                key_query.mark_released(event.key)
+                io.mark_released(event.key)
 
-        left_right = int(key_query.is_key_down(K_d)) - int(key_query.is_key_down(K_a))
-        up_down    = int(key_query.is_key_down(K_s)) - int(key_query.is_key_down(K_w))
+
+        left_right = int(io.is_key_down(K_d)) - int(io.is_key_down(K_a))
+        up_down    = int(io.is_key_down(K_s)) - int(io.is_key_down(K_w))
 
         kinematic_rect.set_velocity((left_right * 400, up_down * 400))
         kinematic_line.set_velocity((left_right * 400, up_down * 400))
-        game_balls[2].set_velocity((left_right * 400,  up_down * 400))
+        kinematic_circle.set_velocity((left_right * 400, up_down * 400))
+        
+        mouse_relo = (
+            io.get_mouse_relative()[0] * TARGET_FPS,
+            io.get_mouse_relative()[1] * TARGET_FPS,
+        )
+        mouse_ball.set_velocity(mouse_relo)
+        mouse_ball.set_position(pygame.mouse.get_pos())
 
-        if key_query.is_key_pressed(K_q):
-            shape_register.delete(kinematic_rect)
+        if io.is_key_pressed(K_q):
+            shape_registry.delete(kinematic_rect)
             
 
 
+        dynamic_circle.set_angular_velocity(random.random() * 50 - 25)
+        dynamic_circle.set_friction(random.random() * 0.2)
+
+
         screen.fill((0, 0, 0, 0))
-
-        for ball in game_balls[:1]:
-            # Add random angular velocity and friction to encourage the ball to
-            # bounce around
-            ball: DynamicCircle
-            ball.set_angular_velocity(random.random() * 50 - 25)
-            ball.set_friction(random.random() * 0.2)
-
-        shape_register.draw_shapes(screen)
+        shape_registry.draw_shapes(screen)
 
         # Make Box2D simulate the physics of our world for one step.
         # Instruct the world to perform a single step of simulation. It is
--- model.py
+++ model.py
@@ -1,15 +1,11 @@
 from __future__ import annotations
 from abc import ABC, abstractmethod
-from typing import List, Tuple, Optional, Set
-from enum import Enum, auto
+from typing import List, Tuple, Set
+from enum import Enum, auto, Flag
 
 import Box2D
 import pygame
-
-# Box2D deals with meters, but we want to display pixels,
-# so define a conversion factor:
-PPM = 20.0  # pixels per meter
-SCREEN_WIDTH, SCREEN_HEIGHT = 640, 480
+from settings import *
 
 
 def pixels_to_box2d(p: float) -> float:
@@ -42,6 +38,7 @@ class KeyQuery:
         self.keys_down = set()
         self.keys_pressed = set()
         self.keys_released = set()
+        self.mouse_relative: Tuple[int, int] = None
     
     def is_key_down(self, key: int):
         return key in self.keys_down
@@ -60,9 +57,15 @@ class KeyQuery:
         self.keys_down.remove(key)
         self.keys_released.add(key)
 
-    def clear(self):
+    def clear_pressed(self):
         self.keys_pressed.clear()
         self.keys_released.clear()
+
+    def mark_mouse_relative(self, rel: Tuple[int, int]):
+        self.mouse_relative = rel
+
+    def get_mouse_relative(self) -> Tuple[int, int]:
+        return self.mouse_relative
 
 
 class Shape(ABC):
@@ -76,9 +79,18 @@ class Shape(ABC):
     def b2_get_shape(self) -> Box2D.b2Shape:
         return self.shape
     
-    def b2_get_fixture(self) -> Optional[Box2D.b2Fixture]:
-        """Only returns a fixture for Dynamic Bodies"""
-        return self.fixture
+    def b2_get_fixture(self) -> Box2D.b2Fixture:
+        if len(self.b2_get_body().fixtures) > 1:
+            print(self.b2_get_body().fixtures)
+        assert len(self.b2_get_body().fixtures) == 1
+
+        return self.b2_get_body().fixtures[0]
+
+    def set_collision_group(self, my_group: Flag, can_collide_with_groups: Flag):
+        self.b2_get_fixture().filterData = Box2D.b2Filter(
+        categoryBits=my_group.value,
+        maskBits=can_collide_with_groups.value
+    )
 
     def get_position(self) -> Tuple[float, float]:
         return flip_y_position(box2d_to_pixels_v(self.b2_get_body().position))
@@ -124,7 +136,7 @@ class Shape(ABC):
         self.b2_get_fixture().restitution = restitution
 
 
-class ShapeRegister:
+class ShapeRegistry:
     def __init__(self, world: Box2D.b2World):
         self.world = world
         self.shapes: Set[Shape] = set()
@@ -195,20 +207,17 @@ class __Circle(Shape):
         elif collision_type == CollisionType.Dynamic:
             self.body: Box2D.b2Body = world.CreateDynamicBody(
                 position=position,
-                shapes=self.shape,
-            )
-            assert collision_info is not None, "Must provide CollisionInfo for dynamic type"
-            self.fixture = self.body.CreateCircleFixture(
-                radius=radius,
-                density=collision_info.density,
-                friction=collision_info.friction,
-                restitution=collision_info.restitution,
-                userData=self
+                fixtures=Box2D.b2FixtureDef(
+                    shape=self.shape,
+                    density=collision_info.density,
+                    friction=collision_info.friction,
+                    restitution=collision_info.restitution,
+                )
             )
         else:
             assert False, "Must use a valid collision type"
         
-        self.body.fixtures[0].userData = self
+        self.b2_get_fixture().userData = self
 
     def draw(self, screen: pygame.Surface):
         # We take the body's transform and multiply it with each
@@ -289,7 +298,6 @@ class __Rectangle(Shape):
         self.shape = Box2D.b2PolygonShape(
             box=size,
         )
-        self.fixture: Box2D.b2Fixture = None
         if collision_type == CollisionType.Static:
             self.body: Box2D.b2Body = world.CreateStaticBody(
                 position=position,
@@ -303,20 +311,17 @@ class __Rectangle(Shape):
         elif collision_type == CollisionType.Dynamic:
             self.body: Box2D.b2Body = world.CreateDynamicBody(
                 position=position,
-                shapes=self.shape,
-            )
-            assert collision_info is not None, "Must provide CollisionInfo for dynamic type"
-            self.fixture = self.body.CreatePolygonFixture(
-                box=size,
-                density=collision_info.density,
-                friction=collision_info.friction,
-                restitution=collision_info.restitution,
-                userData=self
+                fixtures=Box2D.b2FixtureDef(
+                    shape=self.shape,
+                    density=collision_info.density,
+                    friction=collision_info.friction,
+                    restitution=collision_info.restitution,
+                )
             )
         else:
             assert False, "Must use a valid collision type"
         
-        self.body.fixtures[0].userData = self
+        self.b2_get_fixture().userData = self
 
     def draw(self, screen: pygame.Surface):
         # We take the body's transform and multiply it with each
@@ -379,16 +384,17 @@ class __Line(Shape):
             collision_info: CollisionInfo,
             colour: Tuple[int, int, int, int]=(255, 255, 255, 255),
     ):
-        point_a = pixels_to_box2d_v(flip_y_position(point_a))
-        point_b = pixels_to_box2d_v(flip_y_position(point_b))
-        point_a2 = pixels_to_box2d_v(flip_y_position((point_a[0], point_a[1] + 1)))
-        point_b2 = pixels_to_box2d_v(flip_y_position((point_b[0], point_b[1] + 1)))
+        self.is_secretly_polygon = False
+        self.point_a = pixels_to_box2d_v(flip_y_position(point_a))
+        self.point_b = pixels_to_box2d_v(flip_y_position(point_b))
+        self.point_a2 = pixels_to_box2d_v(flip_y_position((point_a[0], point_a[1] + 1)))
+        self.point_b2 = pixels_to_box2d_v(flip_y_position((point_b[0], point_b[1] + 1)))
+
         
         self.colour = colour
         self.shape = Box2D.b2EdgeShape(
-            vertices=[point_a, point_b],
+            vertices=[self.point_a, self.point_b],
         )
-        self.fixture: Box2D.b2Fixture = None
         if collision_type == CollisionType.Static:
             self.body: Box2D.b2Body = world.CreateStaticBody(
                 shapes=self.shape
@@ -398,22 +404,28 @@ class __Line(Shape):
                 shapes=self.shape
             )
         elif collision_type == CollisionType.Dynamic:
-            self.body: Box2D.b2Body = world.CreateDynamicBody(
-                shapes=self.shape
-            )
             assert collision_info is not None, "Must provide CollisionInfo for dynamic type"
-            self.fixture = self.body.CreatePolygonFixture(
-                vertices=[point_a, point_a2, point_b, point_b2],
-                density=collision_info.density,
-                friction=collision_info.friction,
-                restitution=collision_info.restitution,
-                userData=self
+            # We cheat with DynamicLine. It's actually a very small polygon.
+            self.is_secretly_polygon = True
+            self.polygon_shape = Box2D.b2PolygonShape(
+                vertices=[self.point_a, self.point_a2, self.point_b, self.point_b2]
             )
+            self.body: Box2D.b2Body = world.CreateDynamicBody(
+                fixtures=Box2D.b2FixtureDef(
+                    shape=self.polygon_shape,
+                    density=collision_info.density,
+                    friction=collision_info.friction,
+                    restitution=collision_info.restitution,
+                )
+            )
+            print(self.point_a)
+            print(self.point_b)
+            print(self.point_a2)
+            print(self.point_b2)
         else:
             assert False, "Must use a valid collision type"
         
-        self.body.fixtures[0].userData = self
-
+        self.b2_get_fixture().userData = self
 
     def draw(self, screen: pygame.Surface, width: int = 1):
         vertices = [box2d_to_pixels_v(self.body.transform * v) for v in self.shape.vertices]
@@ -426,7 +438,23 @@ class __Line(Shape):
         # the y components.
         vertices = [flip_y_position(v) for v in vertices]
         pygame.draw.line(screen, self.colour, vertices[0], vertices[1], width)
-    
+
+        if self.is_secretly_polygon and False:
+            # We take the body's transform and multiply it with each
+            # vertex, and then convert from meters to pixels with the scale
+            # factor.
+            vertices = [box2d_to_pixels_v(self.body.transform * v) for v in self.polygon_shape.vertices]
+
+            # But wait! It's upside-down! Pygame and Box2D orient their
+            # axes in different ways. Box2D is just like how you learned
+            # in high school, with positive x and y directions going
+            # right and up. Pygame, on the other hand, increases in the
+            # right and downward directions. This means we must flip
+            # the y components.
+            vertices = [flip_y_position(v) for v in vertices]
+
+            pygame.draw.polygon(screen, (255, 255, 255, 100), vertices)
+        
 
 class StaticLine(__Line):
     def __init__(
